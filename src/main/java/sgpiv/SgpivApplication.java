c1ffa7ac9b251a92b528c1dc54088bd16b55de96
MarceB10 <angelbaldome@gmail.com>
2026-05-01 01:49:41 -0300

    feat: initial project setup con Spring Boot

---
+++ src/main/java/sgpiv/SgpivApplication.java
@@ -0,0 +1,13 @@
+package sgpiv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgpivApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgpivApplication.class, args);
+	}
+
+}
