f4bed9af2ba20d262f315f538790a7f48ef2ace1
Rodrigo <rodrigoquichan360@gmail.com>
2026-05-14 13:47:26 -0300

    empresas agregadas en initializer

--- src/main/java/sgpiv/config/DataInitializer.java
+++ src/main/java/sgpiv/config/DataInitializer.java
@@ -55,21 +55,53 @@ public class DataInitializer implements CommandLineRunner {
         if (empresaRepository.count() == 0) {
 
             Empresa empresa1 = new Empresa();
-            empresa1.setRazonSocial("Google");
+            empresa1.setRazonSocial("Patagonia Logística");
             empresa1.setCuit("20-12345678-9");
-            empresa1.setRubro("Tecnologia");
-            empresa1.setEmail("google@gmail.com");
+            empresa1.setRubro("Logística");
+            empresa1.setEmail("contacto@patagonialogistica.com");
             empresa1.setEstadoEmpresa(EstadoEmpresa.INTERESADA);
 
             Empresa empresa2 = new Empresa();
-            empresa2.setRazonSocial("Microsoft");
+            empresa2.setRazonSocial("Premoldeados Viedma");
             empresa2.setCuit("27-98765432-1");
-            empresa2.setRubro("Software");
-            empresa2.setEmail("microsoft@gmail.com");
+            empresa2.setRubro("Construcción");
+            empresa2.setEmail("ventas@premoldeadosviedma.com");
             empresa2.setEstadoEmpresa(EstadoEmpresa.RADICADA);
+
+            Empresa empresa3 = new Empresa();
+            empresa3.setRazonSocial("Frigorífico Río Negro");
+            empresa3.setCuit("30-45678912-3");
+            empresa3.setRubro("Frigorífico");
+            empresa3.setEmail("info@frigorificiorn.com");
+            empresa3.setEstadoEmpresa(EstadoEmpresa.ADJUDICADA);
+
+            Empresa empresa4 = new Empresa();
+            empresa4.setRazonSocial("Aberturas del Sur");
+            empresa4.setCuit("30-22223333-4");
+            empresa4.setRubro("Aberturas de aluminio");
+            empresa4.setEmail("contacto@aberturasdelsur.com");
+            empresa4.setEstadoEmpresa(EstadoEmpresa.RADICADA);
+
+            Empresa empresa5 = new Empresa();
+            empresa5.setRazonSocial("Valle Inferior Agro");
+            empresa5.setCuit("30-99887766-5");
+            empresa5.setRubro("Agroindustria");
+            empresa5.setEmail("administracion@vigro.com");
+            empresa5.setEstadoEmpresa(EstadoEmpresa.INTERESADA);
+
+            Empresa empresa6 = new Empresa();
+            empresa6.setRazonSocial("Maderas Patagónicas");
+            empresa6.setCuit("30-11112222-6");
+            empresa6.setRubro("Aserradero");
+            empresa6.setEmail("ventas@maderaspatagonicas.com");
+            empresa6.setEstadoEmpresa(EstadoEmpresa.BAJA);
 
             empresaRepository.save(empresa1);
             empresaRepository.save(empresa2);
+            empresaRepository.save(empresa3);
+            empresaRepository.save(empresa4);
+            empresaRepository.save(empresa5);
+            empresaRepository.save(empresa6);
 
             System.out.println("Empresas de prueba cargadas");
         }
