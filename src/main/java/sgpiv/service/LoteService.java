267af89bcdff2ad88dc90f844874baa6a44a1851
JairoCrespo05 <crespijairo5@gmail.com>
2026-05-17 13:19:08 -0300

    Defnir Lote HU

---
+++ src/main/java/sgpiv/service/LoteService.java
@@ -0,0 +1,41 @@
+package sgpiv.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import sgpiv.dtos.response.LoteResponseDTO;
+import sgpiv.dtos.request.LoteRequestDTO;
+import sgpiv.model.Lote;
+import sgpiv.repository.LoteRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class LoteService {
+
+    private final LoteRepository loteRepository;
+
+
+    public void definirLote(LoteRequestDTO loteDTO){
+
+        Lote loteNuevo = new Lote(loteDTO.getSuperficie(), loteDTO.getUbicacion(), loteDTO.getPrecio(), loteDTO.getRestricciones());
+
+        loteRepository.save(loteNuevo);
+    }
+
+    public List<LoteResponseDTO> obtenerTodosLosLotes(){
+        List<LoteResponseDTO> lotesResponseDTO = new ArrayList<>();
+        List<Lote> lotes = loteRepository.findAll();
+
+        for (Lote lote: lotes){
+            lotesResponseDTO.add(
+                    new LoteResponseDTO(lote)
+            );
+        }
+
+        return lotesResponseDTO;
+    }
+
+
+}
