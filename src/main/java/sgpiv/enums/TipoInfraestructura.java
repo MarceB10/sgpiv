4085e0310c60f2599003f29f7f82c014b8884539
Rodrigo <rodrigoquichan360@gmail.com>
2026-06-03 19:54:40 -0300

    infraestructuras

---
+++ src/main/java/sgpiv/enums/TipoInfraestructura.java
@@ -0,0 +1,14 @@
+package sgpiv.enums;
+
+public enum TipoInfraestructura {
+    ELECTRICA,
+    AGUA_POTABLE,
+    ALUMBRADO_PUBLICO,
+    GAS_NATURAL,
+    TELECOMUNICACIONES,
+    AGUA_RESIDUAL,
+    RED_CLOACAL,
+    VIAL,
+    SEGURIDAD,
+    ESPACIO_COMUN
+}
